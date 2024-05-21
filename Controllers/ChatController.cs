d76ff53292ef1ea151df3974ac028d6660e852ed
Candela Fernandez <cande.fdz12@gmail.com>
2024-05-21 13:24:17 -0300

    subida

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatRoom.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static Dictionary<int, string> Rooms =
+            new Dictionary<int, string>()
+            {
+                {1, "Cervezas"},
+                {2, "Programacion"},
+                {3, "Moda"}
+            };
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Room(int room)
+        {
+            return View("Room", room);
+        }
+    }
+}
