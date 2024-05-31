c69aeab8ebbf5688bac777ecce8fd8a4622d75a4
Andrea Sosa <andrea.sosa@arg01.local>
2024-05-30 23:59:08 -0300

    Eliminate files

--- ChatRoom/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ChatRoom.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
