736ff16d51b03e681e6cd54860fb16b0537bb730
Ailu Vilches <ailenpereiravilches@gmail.com>
2024-06-02 17:43:08 -0300

    controllers y vistas para el login

---
+++ ChatRoom/Models/RegisterViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatRoom.Models
+{
+    public class RegisterViewModel
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+    }
+}
