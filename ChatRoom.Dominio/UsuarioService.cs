214d715202cbf0db60b699f22e070b81dcbbb08d
Candela Fernandez <cande.fdz12@gmail.com>
2024-06-15 10:42:31 -0300

    historial de usuario, perfil, compras y ventas que habia hecho andre

---
+++ ChatRoom.Dominio/UsuarioService.cs
@@ -0,0 +1,44 @@
+﻿using ChatRoom.Datos;
+using ChatRoom.Datos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatRoom.Dominio
+{
+    public interface IUsuarioService
+    {
+        List<Venta> GetCompras(string id);
+        List<Venta> GetVentas(string id);
+    }
+    public class UsuarioService : IUsuarioService
+    {
+        private SubastaContext _subastaContext;
+
+        public UsuarioService(SubastaContext subastaContext)
+        {
+            _subastaContext = subastaContext;
+        }
+
+        public List<Venta> GetCompras(string id)
+        {
+            var compras = _subastaContext.Venta
+                          .Where(v => v.IdComprador == id)
+                          .ToList();
+
+            return compras;
+
+        }
+        public List<Venta> GetVentas(string id)
+        {
+            var compras = _subastaContext.Venta
+                          .Where(v => v.IdVendedor == id)
+                          .ToList();
+
+            return compras;
+
+        }
+    }
+}
