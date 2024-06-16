40263e7764a80f9442a2dcca34e481b84c397291
Candela Fernandez <cande.fdz12@gmail.com>
2024-06-16 11:29:44 -0300

    se puede subir la imagen y se guarda como varchar en la bd el path de la imagen, falta que se muestre dentro de la sala

--- ChatRoom/Program.cs
+++ ChatRoom/Program.cs
@@ -8,7 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddSignalR();
+builder.Services.AddSignalR(options =>
+{
+    options.MaximumReceiveMessageSize = 1024 * 1024; 
+});
+
 builder.Services.AddScoped<SubastaContext>();
 builder.Services.AddScoped<ISalaService, SalaService>();
 builder.Services.AddScoped<IOfertumService, OfertumService>();
